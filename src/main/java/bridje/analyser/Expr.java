df9f243e289c07f7f6f3dd745028f2a7f3d6b24f
James Henderson <james@jarohen.me.uk>
2017-03-01 14:09:05 +0000

    Adding in a StringExpr

--- src/main/java/bridje/analyser/Expr.java
+++ src/main/java/bridje/analyser/Expr.java
@@ -1,4 +1,20 @@
 package bridje.analyser;
 
+import bridje.reader.Range;
+
 public abstract class Expr {
+    public final Range range;
+
+    Expr(Range range) {
+        this.range = range;
+    }
+
+    public static final class StringExpr extends Expr {
+        public final String value;
+
+        public StringExpr(Range range, String value) {
+            super(range);
+            this.value = value;
+        }
+    }
 }
