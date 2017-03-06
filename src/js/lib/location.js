bd0f7e50ba1587aaa85f05553582a5d352830b46
James Henderson <james@jarohen.me.uk>
2017-03-06 21:54:18 +0000

    loc toStrings

--- src/js/lib/location.js
+++ src/js/lib/location.js
@@ -3,12 +3,20 @@ var Record = require('immutable').Record;
 var Location = Record({line: 1, col: 1});
 var Range = Record({start: null, end: null});
 
+Location.prototype.toString = function() {
+  return `L${this.line}C${this.col}`;
+};
+
 function range(start, end) {
   return new Range({
     start: start,
     end: end.update('col', col => col - 1)
   });
 }
+
+Range.prototype.toString = function() {
+  return `${this.start}-${this.end}`;
+};
 
 var newLoc = new Location();
 
