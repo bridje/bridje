a7a7587f54f8f7baa5c93763e978f01c1c059737
James Henderson <james@jarohen.me.uk>
2017-03-18 22:07:46 +0000

    giving exprs and forms an explicit type field

--- src/js/expr.js
+++ src/js/expr.js
@@ -7,17 +7,24 @@ var StringExpr = Record({str: null});
 var IntExpr = Record({int: null});
 var FloatExpr = Record({float: null});
 var VectorExpr = Record({exprs: null});
+var SetExpr = Record({exprs: null});
 var RecordEntry = Record({key: null, value: null});
 var RecordExpr = Record({entries: null});
-var SetExpr = Record({exprs: null});
 
 Expr.prototype.toString = function() {return this.expr.toString();};
 
 BoolExpr.prototype.toBool = function() {return `(BoolExpr "${this.bool}")`;};
+BoolExpr.prototype.exprType = 'bool';
 StringExpr.prototype.toString = function() {return `(StringExpr "${this.str}")`;};
+StringExpr.prototype.exprType = 'string';
 IntExpr.prototype.toString = function() {return `(IntExpr ${this.int})`;};
+IntExpr.prototype.exprType = 'int';
 FloatExpr.prototype.toString = function() {return `(FloatExpr ${this.float})`;};
+FloatExpr.prototype.exprType = 'float';
 VectorExpr.prototype.toString = function() {return `(VectorExpr ${this.exprs.join(' ')})`;};
+VectorExpr.prototype.exprType = 'vector';
+SetExpr.prototype.toString = function() {return `(SetExpr ${this.exprs.join(' ')})`;};
+SetExpr.prototype.exprType = 'set';
 RecordEntry.prototype.toString = function() {return `${this.key} ${this.value}`;};
 RecordExpr.prototype.toString = function() {return `(RecordExpr ${this.entries.join(', ')})`;};
-SetExpr.prototype.toString = function() {return `(SetExpr ${this.exprs.join(' ')})`;};
+RecordExpr.prototype.exprType = 'record';
--- src/js/form.js
+++ src/js/form.js
@@ -6,28 +6,42 @@ var BoolForm = Record({bool: null});
 var StringForm = Record({str: null});
 var IntForm = Record({int: null});
 var FloatForm = Record({float: null});
-var ListForm = Record({forms: null});
 var VectorForm = Record({forms: null});
+var SetForm = Record({forms: null});
 var RecordEntry = Record({key: null, value: null});
 var RecordForm = Record({entries: null});
-var SetForm = Record({forms: null});
+var ListForm = Record({forms: null});
 var SymbolForm = Record({sym: null});
 
 Form.prototype.toString = function() {return this.form.toString();};
 
 BoolForm.prototype.toString = function() {return `(BoolForm "${this.bool}")`;};
+BoolForm.prototype.formType = 'bool';
 StringForm.prototype.toString = function() {return `(StringForm "${this.str}")`;};
+StringForm.prototype.formType = 'string';
 IntForm.prototype.toString = function() {return `(IntForm ${this.int})`;};
+IntForm.prototype.formType = 'int';
 FloatForm.prototype.toString = function() {return `(FloatForm ${this.float})`;};
-SymbolForm.prototype.toString = function() {return `(SymbolForm ${this.sym})`;};
-ListForm.prototype.toString = function() {return `(ListForm ${this.forms.join(' ')})`;};
+FloatForm.prototype.formType = 'float';
+
 VectorForm.prototype.toString = function() {return `(VectorForm ${this.forms.join(' ')})`;};
+VectorForm.prototype.formType = 'vector';
+SetForm.prototype.toString = function() {return `(SetForm ${this.forms.join(' ')})`;};
+SetForm.prototype.formType = 'set';
+
 RecordEntry.prototype.toString = function() {return `${this.key} ${this.value}`;};
 RecordForm.prototype.toString = function() {return `(RecordForm ${this.entries.join(', ')})`;};
-SetForm.prototype.toString = function() {return `(SetForm ${this.forms.join(' ')})`;};
+RecordForm.prototype.formType = 'record';
+
+ListForm.prototype.toString = function() {return `(ListForm ${this.forms.join(' ')})`;};
+ListForm.prototype.formType = 'list';
+SymbolForm.prototype.toString = function() {return `(SymbolForm ${this.sym})`;};
+SymbolForm.prototype.formType = 'symbol';
 
 module.exports = {
   Form,
-  BoolForm, StringForm, IntForm, FloatForm, SymbolForm,
-  ListForm, RecordEntry, RecordForm, VectorForm, SetForm
+  BoolForm, StringForm, IntForm, FloatForm,
+  VectorForm, SetForm,
+  RecordEntry, RecordForm,
+  ListForm, SymbolForm
 };
