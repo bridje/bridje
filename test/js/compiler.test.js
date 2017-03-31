bf5143460ea91a3263dff9ceb462e1a37571ad9c
James Henderson <james@jarohen.me.uk>
2017-03-31 09:16:46 +0100

    Adding a small test harness into the compiler test

--- test/js/compiler.test.js
+++ test/js/compiler.test.js
@@ -7,6 +7,15 @@ const {Record} = im;
 const vm = require('vm');
 const assert = require('assert');
 
+function evalCode(code) {
+  // console.log(`(function (_runtime, _im) {return ${code};})`);
+  return new vm.Script(`(function (_runtime, _im) {return ${code};})`).runInThisContext()(runtime, im);
+}
+
+function compileForm(form, env, nsEnv) {
+  return compileExpr(env, nsEnv, analyseForm(env, nsEnv, readForms(form).first()));
+}
+
 describe('compiler', () => {
   it('compiles a NS', () => {
     let expr = analyseForm(null, null, readForms('(def foo-bar ["hello world!"])').first());
@@ -14,5 +23,11 @@ describe('compiler', () => {
     let compiledNS = new vm.Script(compileNS(null, compileResult.nsEnv, compileResult.code)).runInThisContext()(runtime, im).f();
     assert.deepEqual(compiledNS.exports.getIn(['foo-bar', 'value']).toJS(), ['hello world!']);
   });
+
+  it('compiles a record', () => {
+    const result = evalCode(compileForm('{a 1, b 2}').code);
+    console.log(result);
+  });
+
 
 });
