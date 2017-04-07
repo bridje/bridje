f49e390b44ea0d24bb0f8982cfa38354d7dd088f
James Henderson <james@jarohen.me.uk>
2017-04-07 23:51:26 +0100

    Loading namespaces in the right order!

--- src/js/eval.js
+++ src/js/eval.js
@@ -1,6 +1,6 @@
 const {readForms} = require('./reader');
 const im = require('immutable');
-const {Record, List, Map} = im;
+const {Record, List, Map, Set} = im;
 var process = require('process');
 const e = require('./env');
 const a = require('./analyser');
@@ -11,9 +11,8 @@ const runtime = require('./runtime');
 module.exports = function(nsLoader) {
   var envManager = e.envManager();
 
-  function envRequire(env, ns, str) {
-    const forms = readForms(str);
-    const {nsEnv, codes} = forms.shift().reduce(
+  function envRequire(env, nsHeader, forms) {
+    const {nsEnv, codes} = forms.reduce(
       ({nsEnv, codes}, form) => {
         const expr = a.analyseForm(env, nsEnv, form);
         let code;
@@ -21,22 +20,68 @@ module.exports = function(nsLoader) {
         return {nsEnv, codes: codes.push(code)};
       },
       {
-        nsEnv: a.resolveNSHeader(env, a.readNSHeader(ns, forms.first())),
+        nsEnv: a.resolveNSHeader(env, nsHeader),
         codes: new List()
       });
 
     const {exports} = new vm.Script(c.compileNS(env, nsEnv, codes.join("\n")))
           .runInThisContext()(runtime, im)(nsEnv);
 
-    return env.setIn(['nsEnvs', ns], nsEnv.set('exports', exports));
+    return env.setIn(['nsEnvs', nsEnv.ns], nsEnv.set('exports', exports));
+  }
+
+  function resolveNSsAsync(env, ns, str) {
+    const preLoadedNSs = Set(env.nsEnvs.keySeq());
+
+    function readNS(ns, str) {
+      const forms = readForms(str);
+      const nsHeader = a.readNSHeader(ns, forms.first());
+      const dependentNSs = Set(nsHeader.aliases.valueSeq()).union(nsHeader.refers.valueSeq().flatten());
+      return {nsHeader, dependentNSs, forms: forms.shift()};
+    }
+
+    function resolveQueueAsync({loadedNSs, nsLoadOrder, queuedNSs}) {
+      if (queuedNSs.isEmpty()) {
+        return nsLoadOrder.map(ns => loadedNSs.get(ns));
+      } else {
+        return Promise.all(queuedNSs.map(ns => nsLoader.resolveNSAsync(ns).then(str => ({ns, str})))).then(results => {
+          queuedNSs = Set();
+          results.forEach(({ns, str}) => {
+            const {nsHeader, dependentNSs, forms} = readNS(ns, str);
+            loadedNSs = loadedNSs.set(ns, Map({nsHeader, forms}));
+            nsLoadOrder = nsLoadOrder.unshift(ns);
+            queuedNSs = queuedNSs.delete(ns).union(dependentNSs.subtract(queuedNSs).subtract(preLoadedNSs));
+          });
+
+          return resolveQueueAsync({loadedNSs, nsLoadOrder, queuedNSs});
+        });
+      }
+    }
+
+    if (str !== undefined) {
+      const {nsHeader, dependentNSs, forms} = readNS(ns, str);
+      return resolveQueueAsync({
+        loadedNSs: Map({ns: {nsHeader, forms}}),
+        nsLoadOrder: List.of(ns),
+        queuedNSs: Set.of(dependentNSs)
+      });
+    } else {
+      return resolveQueueAsync({
+          loadedNSs: Map(),
+          nsLoadOrder: List(),
+          queuedNSs: Set.of(ns)
+      });
+    }
   }
 
   function envRequireAsync(env, ns, str) {
-    // TODO require other namespaces as necessary
-
     if (env.nsEnvs.get(ns) === undefined) {
-      const strAsync = str !== undefined ? Promise.resolve(str) : nsLoader.resolveNSAsync(ns);
-      return strAsync.then(str => envRequire(env, ns, str));
+      return resolveNSsAsync(env, ns, str).then(loadedNSs => {
+        return loadedNSs.reduce((env, loadedNS) => {
+          const {nsHeader, forms} = loadedNS.toObject();
+          return envRequire(env, nsHeader, forms);
+        }, env);
+      });
     } else {
       return Promise.resolve(env);
     }
--- test/js/eval.test.js
+++ test/js/eval.test.js
@@ -1,4 +1,6 @@
 var e = require('../../src/js/eval');
+var {NSHeader} = require('../../src/js/analyser');
+var {readForms} = require('../../src/js/reader');
 const fakeLoader = require('./fake-ns-loader');
 const realLoader = require('../../src/js/ns-loader')(["src/brj", "test/brj"]);
 
@@ -10,8 +12,19 @@ describe('eval', () => {
       'bridje.kernel': `(ns bridje.kernel)`
     }));
 
-    const newEnv = eval.envRequire(eval.currentEnv(), 'bridje.kernel.test', `(ns bridje.kernel.test) (def hello ["hello world"])`);
+    const newEnv = eval.envRequire(eval.currentEnv(), NSHeader({ns:'bridje.kernel.test'}), readForms(`(def hello ["hello world"])`));
     assert.deepEqual(newEnv.nsEnvs.get('bridje.kernel.test').exports.get('hello').value.toJS(), ['hello world']);
+  });
+
+  it ('requires in another namespace', async () => {
+    const eval = e(fakeLoader({
+      'bridje.kernel': `(ns bridje.kernel)`,
+      'bridje.kernel.foo': `(ns bridje.kernel.foo) (def (flip x y) [y x])`,
+      'bridje.kernel.bar': `(ns bridje.kernel.bar {refers {bridje.kernel.foo [flip]}}) (def hello (flip 4 3))`
+    }));
+
+    const newEnv = await eval.envRequireAsync(await eval.loaded, 'bridje.kernel.bar');
+    assert.deepEqual(newEnv.nsEnvs.get('bridje.kernel.bar').exports.get('hello').value.toJS(), [3, 4]);
   });
 
   it ('runs a main', async () => {
