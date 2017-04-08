bf01ea4311ae7bf6ec7a44526d2cc2b8e6aad421
James Henderson <james@jarohen.me.uk>
2017-04-08 14:31:10 +0100

    Adding subExprs to exprs s.t. we can easily walk an expr

--- src/js/expr.js
+++ src/js/expr.js
@@ -1,4 +1,4 @@
-const {Record} = require('immutable');
+const {Record, List} = require('immutable');
 
 const BoolExpr = Record({range: null, bool: null});
 const StringExpr = Record({range: null, str: null});
@@ -12,7 +12,7 @@ const RecordExpr = Record({range: null, entries: null});
 const IfExpr = Record({range: null, testExpr: null, thenExpr: null, elseExpr: null});
 
 const LocalVarExpr = Record({range: null, name: null, localVar: null});
-const VarExpr = Record({range: null, var: null});
+const VarExpr = Record({range: null, var: null, alias: null});
 const JSGlobalExpr = Record({range: null, path: null});
 
 const LetBinding = Record({localVar: null, expr: null});
@@ -25,41 +25,76 @@ const DefExpr = Record({range: null, sym: null, params: null, body: null});
 
 BoolExpr.prototype.toString = function() {return `(BoolExpr ${this.bool})`;};
 BoolExpr.prototype.exprType = 'bool';
+BoolExpr.prototype.subExprs = function() {return List.of(this);};
+
 StringExpr.prototype.toString = function() {return `(StringExpr "${this.str}")`;};
 StringExpr.prototype.exprType = 'string';
+StringExpr.prototype.subExprs = function() {return List.of(this);};
+
 IntExpr.prototype.toString = function() {return `(IntExpr ${this.int})`;};
 IntExpr.prototype.exprType = 'int';
+IntExpr.prototype.subExprs = function() {return List.of(this);};
+
 FloatExpr.prototype.toString = function() {return `(FloatExpr ${this.float})`;};
 FloatExpr.prototype.exprType = 'float';
+FloatExpr.prototype.subExprs = function() {return List.of(this);};
+
 VectorExpr.prototype.toString = function() {return `(VectorExpr ${this.exprs.join(' ')})`;};
 VectorExpr.prototype.exprType = 'vector';
+VectorExpr.prototype.subExprs = function() {return this.exprs.flatMap(e => e.subExprs());};
+
 SetExpr.prototype.toString = function() {return `(SetExpr ${this.exprs.join(' ')})`;};
 SetExpr.prototype.exprType = 'set';
+SetExpr.prototype.subExprs = function() {return this.exprs.flatMap(e => e.subExprs());};
+
 RecordEntry.prototype.toString = function() {return `${this.key} ${this.value}`;};
 RecordExpr.prototype.toString = function() {return `(RecordExpr {${this.entries.join(', ')})}`;};
 RecordExpr.prototype.exprType = 'record';
+RecordExpr.prototype.subExprs = function() {
+  return List.of(this)
+    .concat(this.entries.flatMap(e => e.key.subExprs().concat(e.value.subExprs())));};
 
 IfExpr.prototype.toString = function() {return `(IfExpr ${this.testExpr} ${this.thenExpr} ${this.elseExpr})`;};
 IfExpr.prototype.exprType = 'if';
+IfExpr.prototype.subExprs = function() {
+  return List.of(this)
+    .concat(this.testExpr.subExprs())
+    .concat(this.thenExpr.subExprs())
+    .concat(this.elseExpr.subExprs());
+};
 
 LocalVarExpr.prototype.toString = function() {return `(LocalVarExpr ${this.name})`;};
 LocalVarExpr.prototype.exprType = 'localVar';
+LocalVarExpr.prototype.subExprs = function() {return List.of(this);};
+
 VarExpr.prototype.toString = function() {return `(VarExpr ${this.var.ns}/${this.var.name})`;};
 VarExpr.prototype.exprType = 'var';
+VarExpr.prototype.subExprs = function() {return List.of(this);};
+
 JSGlobalExpr.prototype.toString = function() {return `(JSGlobal ${this.path.join('.')})`;};
 JSGlobalExpr.prototype.exprType = 'jsGlobal';
+JSGlobalExpr.prototype.subExprs = function() {return List.of(this);};
 
 LetBinding.prototype.toString = function() {return `${this.name} ${this.expr}`;};
 LetExpr.prototype.toString = function() {return `(LetExpr [${this.bindings.join(', ')}] ${this.body})`;};
 LetExpr.prototype.exprType = 'let';
+LetExpr.prototype.subExprs = function() {
+  return List.of(this)
+    .concat(this.bindings.flatMap(e => e.expr.subExprs().concat(e.value.subExprs())))
+    .concat(this.body.subExprs());
+};
 
 FnExpr.prototype.toString = function() {return `(FnExpr (${this.params.map(p => p.name).join(' ')}) ${this.body})`;};
 FnExpr.prototype.exprType = 'fn';
+FnExpr.prototype.subExprs = function() {return List.of(this).concat(this.body.subExprs());};
+
 CallExpr.prototype.toString = function() {return `(CallExpr ${this.exprs.join(' ')})`;};
 CallExpr.prototype.exprType = 'call';
+CallExpr.prototype.subExprs = function() {return this.exprs.flatMap(e => e.subExprs());};
 
 DefExpr.prototype.toString = function() {return `(DefExpr ${this.sym} ${this.body})`;};
 DefExpr.prototype.exprType = 'def';
+DefExpr.prototype.subExprs = function() {return List.of(this).concat(this.body.subExprs());};
 
 module.exports = {
   BoolExpr, StringExpr, IntExpr, FloatExpr,
