a096345915f5a02f09ca5b56bbea749b4dba23ef
James Henderson <james@jarohen.me.uk>
2017-04-12 09:31:59 +0100

    WIP: starting to parse CLI args

--- src/js/bridje.js
+++ src/js/bridje.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 
-const ev = require('./eval')(['src/brj', 'src/js/test', 'test/brj']);
-const {List} = require('immutable');
-const argv = List(process.argv).slice(2);
+const cli = require('commander')
+const process = require('process');
 
-ev.runMain(argv.first(), argv.shift());
+const cmd = new cli.Command();
+
+cmd.command('build <entry-namespaces...>')
+  .option('-t, --target-path <dir>', 'sets the target path for builds')
+  .option('-p, --path <dir>[:<dir>...]', 'sets the search path for Bridje namespaces')
+  .action(function (entryNamespaces, options) {
+    console.log('yo!', entryNamespaces, options);
+  });
+
+cmd.command('run <main-ns> [args...]')
+  .option('-p, --path <dir>[:<dir>...]', 'sets the search path for Bridje namespaces')
+  .option('-r, --repl [<host>:]port', 'starts a REPL server on the given interface/port')
+  .action(function(mainNS, args, options) {
+    console.log('sup!', mainNS, args, options);
+  });
+
+cmd.parse(process.argv);
