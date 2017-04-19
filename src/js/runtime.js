72cd638cfa978b469d4c7c7d2a11e45e3ce12548
James Henderson <james@jarohen.me.uk>
2017-04-19 09:01:25 +0100

    some thoughts on how to split up compiling namespaces

--- src/js/runtime.js
+++ src/js/runtime.js
@@ -88,6 +88,32 @@ function nsDependents(nsHeader) {
   return Set(nsHeader.aliases.valueSeq()).union(nsHeader.refers.valueSeq().flatten());
 }
 
+/*
+  we want something that, given forms, returns a seq of namespaces with headers
+  and compiledexprs - then, it's up to something else to write the file, eval
+  into a namespace, etc those might all be in this file, of course, but I
+  suspect that's where the split is.
+*/
+
+/*
+  ah, macros in the same namespace. arse. so looks like we are going to have to
+  compile a form at a time - at least in node. maybe we can do the refers for
+  the namespace as a whole. or, maybe we can bring in _everything_ that _could_
+  be aliased. might not do so well for the dead code elimination though.
+*/
+
+/*
+  so what's the pipeline now? pretty much has to be that each form gets
+  analysed, then compiled, then eval'd. can all get written out together. we
+  /could/ do it lazily - only compile out whichever forms are needed for the
+  macros - which wouldn't impact the vast majority of namespaces, in practice.
+
+  so, at the moment, we can run through the NS in order - if a form refers to a
+  var within the namespace which is a macro, we could compile the namespace so
+  far and eval it. think this has the advantage that we can retro-fit it, too.
+*/
+
+
 module.exports = function(nsIO) {
   var queue = EnvQueue();
 
@@ -96,13 +122,13 @@ module.exports = function(nsIO) {
 
     function resolveNSAsync(ns) {
       return nsIO.resolveNSAsync(ns, 'brj').catch(e => null).then(brj => {
-          if (brj == null) {
-            return Promise.reject(`Can't find namespace '${ns}'`);
-          } else {
-            const {nsHeader, forms} = readNS(ns, brj);
-            return {ns, nsHeader, forms};
-          }
-        });
+        if (brj == null) {
+          return Promise.reject(`Can't find namespace '${ns}'`);
+        } else {
+          const {nsHeader, forms} = readNS(ns, brj);
+          return {ns, nsHeader, forms};
+        }
+      });
     }
 
     function resolveQueueAsync({loadedNSs, nsLoadOrder, queuedNSs}) {
