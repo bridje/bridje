74310658ebbc099a87d34788d8e19b500db46573
James Henderson <james@jarohen.me.uk>
2017-04-22 17:53:08 +0100

    splitting out the compiler tests into different sections

--- test/js/compiler.test.js
+++ test/js/compiler.test.js
@@ -27,69 +27,75 @@ function compileForm(form, nsEnv, env) {
 }
 
 describe('compiler', () => {
-  it('loads a vector', () => {
-    const result = evalCompiledForm(compileForm('["hello world!"]').compiledForm);
-    assert.deepEqual(result.toJS(), ['hello world!']);
+  describe('forms', () => {
+    it('loads a vector', () => {
+      const result = evalCompiledForm(compileForm('["hello world!"]').compiledForm);
+      assert.deepEqual(result.toJS(), ['hello world!']);
+    });
+
+    it('loads a record', () => {
+      const result = compileForm('(def record {a 1, b 2})', barNSEnv);
+      const loadNS = evalNSCode(List.of(result.compiledForm), result.nsEnv);
+      assert.deepEqual(loadNS(result.nsEnv).exports.get('record').value.toJS(), {a: 1, b: 2});
+    });
+
+    it ('loads a let', () => {
+      const result = evalCompiledForm(compileForm(`(let [x 1, y 2] [x y])`).compiledForm);
+      assert.deepEqual(result.toJS(), [1, 2]);
+    });
+
+    it ('loads a double hello', () => {
+      const r0 = compileForm(`(def hello "hello")`, new NSEnv());
+      const r1 = compileForm(`(def double [hello hello])`, r0.nsEnv);
+      const result = evalCompiledForm(`(function () {${r0.compiledForm}\n${r1.compiledForm} \n return double;})()`);
+      assert.deepEqual(result.toJS(), ['hello', 'hello']);
+    });
+
+    it ('loads a fn', () => {
+      const expr = evalCompiledForm(compileForm(`(fn (x y) [y x])`).compiledForm);
+      assert.deepEqual(expr(3, 4).toJS(), [4, 3]);
+    });
+
+    it ('loads a call', () => {
+      const expr = evalCompiledForm(compileForm(`((fn (x y) [y x]) 3 4)`).compiledForm);
+      assert.deepEqual(expr.toJS(), [4, 3]);
+    });
+
+    it ('loads a defined function', () => {
+      const def = compileForm(`(def (flip x y) [y x])`, new NSEnv());
+      const expr = compileForm(`(flip 3 4)`, def.nsEnv);
+      const result = evalCompiledForm(`(function () {${def.compiledForm} \n return ${expr.compiledForm};})()`);
+      assert.deepEqual(result.toJS(), [4, 3]);
+    });
+
+    it ('execs a JS global function', () => {
+      const expr = evalCompiledForm(compileForm(`(js/process.cwd)`).compiledForm);
+      assert.equal(expr, process.cwd());
+    });
   });
 
-  it('loads a record', () => {
-    const result = compileForm('(def record {a 1, b 2})', barNSEnv);
-    const loadNS = evalNSCode(List.of(result.compiledForm), result.nsEnv);
-    assert.deepEqual(loadNS(result.nsEnv).exports.get('record').value.toJS(), {a: 1, b: 2});
+  describe('node namespaces', () => {
+    it ('calls a fn referred in from another NS', () => {
+      const result = compileForm(`(def flipped (flip 3 4))`, barNSEnv, fooEnv);
+      const loadNS = evalNSCode(List.of(result.compiledForm), result.nsEnv, fooEnv);
+
+      assert.deepEqual(loadNS(barNSEnv).exports.get('flipped').value.toJS(), [4, 3]);
+    });
+
+    it('calls a function in another namespace through its alias', () => {
+      const result = compileForm(`(def flipped (foo/flip 3 4))`, barNSEnv, fooEnv);
+      const loadNS = evalNSCode(List.of(result.compiledForm), result.nsEnv, fooEnv);
+
+      assert.deepEqual(loadNS(barNSEnv).exports.get('flipped').value.toJS(), [4, 3]);
+    });
   });
 
-  it ('loads a let', () => {
-    const result = evalCompiledForm(compileForm(`(let [x 1, y 2] [x y])`).compiledForm);
-    assert.deepEqual(result.toJS(), [1, 2]);
-  });
-
-  it ('loads a double hello', () => {
-    const r0 = compileForm(`(def hello "hello")`, new NSEnv());
-    const r1 = compileForm(`(def double [hello hello])`, r0.nsEnv);
-    const result = evalCompiledForm(`(function () {${r0.compiledForm}\n${r1.compiledForm} \n return double;})()`);
-    assert.deepEqual(result.toJS(), ['hello', 'hello']);
-  });
-
-  it ('loads a fn', () => {
-    const expr = evalCompiledForm(compileForm(`(fn (x y) [y x])`).compiledForm);
-    assert.deepEqual(expr(3, 4).toJS(), [4, 3]);
-  });
-
-  it ('loads a call', () => {
-    const expr = evalCompiledForm(compileForm(`((fn (x y) [y x]) 3 4)`).compiledForm);
-    assert.deepEqual(expr.toJS(), [4, 3]);
-  });
-
-  it ('loads a defined function', () => {
-    const def = compileForm(`(def (flip x y) [y x])`, new NSEnv());
-    const expr = compileForm(`(flip 3 4)`, def.nsEnv);
-    const result = evalCompiledForm(`(function () {${def.compiledForm} \n return ${expr.compiledForm};})()`);
-    assert.deepEqual(result.toJS(), [4, 3]);
-  });
-
-  it ('execs a JS global function', () => {
-    const expr = evalCompiledForm(compileForm(`(js/process.cwd)`).compiledForm);
-    assert.equal(expr, process.cwd());
-  });
-
-  it ('calls a fn referred in from another NS', () => {
-    const result = compileForm(`(def flipped (flip 3 4))`, barNSEnv, fooEnv);
-    const loadNS = evalNSCode(List.of(result.compiledForm), result.nsEnv, fooEnv);
-
-    assert.deepEqual(loadNS(barNSEnv).exports.get('flipped').value.toJS(), [4, 3]);
-  });
-
-  it('calls a function in another namespace through its alias', () => {
-    const result = compileForm(`(def flipped (foo/flip 3 4))`, barNSEnv, fooEnv);
-    const loadNS = evalNSCode(List.of(result.compiledForm), result.nsEnv, fooEnv);
-
-    assert.deepEqual(loadNS(barNSEnv).exports.get('flipped').value.toJS(), [4, 3]);
-  });
-
-  it('exports a webpack var', () => {
-    const result = compileForm(`(def hello ["hello world"])`, new NSEnv());
+  describe ('web', () => {
+    it('exports a webpack var', () => {
+      const result = compileForm(`(def hello ["hello world"])`, new NSEnv());
       const webNS = evalWebNSCode(List.of(result.compiledForm), result.nsEnv, fooEnv, Map({}));
 
-    assert.deepEqual(webNS.get('hello').value.toJS(), ['hello world']);
+      assert.deepEqual(webNS.get('hello').value.toJS(), ['hello world']);
+    });
   });
 });
