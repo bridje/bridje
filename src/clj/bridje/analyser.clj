36cbcc80d21ccb4a2b27e33baf7952086c70da9f
James Henderson <james@jarohen.me.uk>
2017-09-27 14:10:32 +0100

    Basic monadic parsing, analysing an `if`

--- src/clj/bridje/analyser.clj
+++ src/clj/bridje/analyser.clj
@@ -1,16 +1,57 @@
 (ns bridje.analyser)
 
+(defn parse-forms [forms parser]
+  (first (parser forms)))
+
+(defn pure [v]
+  (fn [forms]
+    [v forms]))
+
+(defmacro do-parse {:style/indent 1} [bindings & body]
+  (if-let [[binding value & more-bindings] (seq bindings)]
+    `(fn [forms#]
+       (let [[~binding more-forms#] (~value forms#)]
+         ((do-parse ~more-bindings ~@body) more-forms#)))
+
+    `(do ~@body)))
+
+(defn first-form-parser [p]
+  (fn [forms]
+    (if-let [[form & more-forms] (seq forms)]
+      [(p form) more-forms]
+      (throw (ex-info "TODO: expected form")))))
+
 (defn analyse [env ns-env {:keys [form-type forms loc-range] :as form}]
-  (case form-type
-    :string {:expr-type :string, :string (:string form)}
-    :bool {:expr-type :bool, :bool (:bool form)}
-    :vector {:expr-type :vector, :exprs (map #(analyse env ns-env %) forms)}
-    :set {:expr-type :set, :exprs (map #(analyse env ns-env %) forms)}
-    :record (cond
-              (pos? (mod (count forms) 2)) (throw (ex-info "Record requires even number of forms" {:loc-range loc-range}))
-              :else {:expr-type :record
-                     :entries (for [[{:keys [form-type loc-range] :as k-form} v-form] (partition 2 forms)]
-                                (cond
-                                  (not= :symbol form-type) (throw (ex-info "Expected symbol as key in record" {:loc-range loc-range}))
-                                  (some? (:ns k-form)) (throw (ex-info "Unexpected namespaced symbol as key in record" {:loc-range loc-range}))
-                                  :else [(:sym k-form) (analyse env ns-env v-form)]))})))
+  (merge {:loc-range loc-range}
+         (case form-type
+           :string {:expr-type :string, :string (:string form)}
+           :bool {:expr-type :bool, :bool (:bool form)}
+           :vector {:expr-type :vector, :exprs (map #(analyse env ns-env %) forms)}
+           :set {:expr-type :set, :exprs (map #(analyse env ns-env %) forms)}
+           :record (cond
+                     (pos? (mod (count forms) 2)) (throw (ex-info "Record requires even number of forms" {:loc-range loc-range}))
+                     :else {:expr-type :record
+                            :entries (for [[{:keys [form-type loc-range] :as k-form} v-form] (partition 2 forms)]
+                                       (cond
+                                         (not= :symbol form-type) (throw (ex-info "Expected symbol as key in record" {:loc-range loc-range}))
+                                         (some? (:ns k-form)) (throw (ex-info "Unexpected namespaced symbol as key in record" {:loc-range loc-range}))
+                                         :else [(:sym k-form) (analyse env ns-env v-form)]))})
+           :list (if (seq forms)
+                   (let [[first-form & more-forms] forms
+                         expr-parser (first-form-parser (fn [form]
+                                                          (analyse env ns-env form)))]
+                     (case (:form-type first-form)
+                       :symbol (if (nil? (:ns first-form))
+                                 (case (keyword (:sym first-form))
+                                   :if (parse-forms more-forms
+                                                    (do-parse [pred-expr expr-parser
+                                                               then-expr expr-parser
+                                                               else-expr expr-parser]
+                                                      (pure {:expr-type :if
+                                                             :pred-expr pred-expr
+                                                             :then-expr then-expr
+                                                             :else-expr else-expr}))))
+
+                                 (throw (ex-info "niy" {})))))
+
+                   (throw (ex-info "niy" {}))))))
