0da58abecef4134a9ebd46a281242800afab9312
James Henderson <james@jarohen.me.uk>
2017-10-20 13:51:43 +0100

    WIP: adding a symbol type to the bridje.runtime

--- src/clj/bridje/runtime.clj
+++ src/clj/bridje/runtime.clj
@@ -1,3 +1,12 @@
 (ns bridje.runtime)
 
 (defrecord ADT [adt-type params])
+
+(deftype Symbol [ns sym])
+
+(def ->Symbol
+  (-> (fn
+        ([sym] (Symbol. nil sym))
+        ([ns sym] (Symbol. ns sym)))
+
+      memoize))
