5190f582aa2c2fe6eadc191c08fe6c20d20ef116
James Henderson <james@jarohen.me.uk>
2017-10-23 18:24:58 +0100

    Refactoring the syntax-quoter

--- src/clj/bridje/quoter.clj
+++ src/clj/bridje/quoter.clj
@@ -2,22 +2,24 @@
   (:require [bridje.analyser :as analyser]
             [bridje.util :as u]))
 
-(defn sym-form [sym]
-  {:form-type :symbol
-   :sym (name sym)})
-
-(defn quote-mk-form [form-type]
-  (let [sym (symbol (str "->" (name (u/form-adt-kw form-type))))]
-    {:form-type :namespaced-symbol
-     :ns 'bridje.forms
-     :sym sym}))
+(defn quoted-form [form-type params]
+  {:form-type :list
+   :forms [{:form-type :namespaced-symbol
+            :ns 'bridje.forms
+            :sym (symbol (str "->" (name (u/form-adt-kw form-type))))}
+           (merge {:form-type :record
+                   :forms (->> params
+                               (into [] (mapcat (fn [[k v]]
+                                                  [{:form-type :symbol
+                                                    :sym (name k)}
+                                                   v]))))})]})
 
 (defn expand-syntax-quotes [form {:keys [current-ns] :as env}]
   (letfn [(syntax-quote-form [{:keys [form-type forms] :as form} {:keys [splice?]}]
             (let [quoted-form (case form-type
                                 :syntax-quote (-> (:form form)
-                                                  (syntax-quote-form {:env env})
-                                                  (syntax-quote-form {:env env}))
+                                                  (syntax-quote-form {:splice? false})
+                                                  (syntax-quote-form {:splice? false}))
 
                                 :unquote (:form form)
 
@@ -26,36 +28,35 @@
                                                     (throw (ex-info "unquote-splicing used outside of collection" {:form form})))
 
                                 :quote {:form-type :quote
-                                        :form (syntax-quote-form (:form form) {:env env})}
+                                        :form (syntax-quote-form (:form form) {:splice? false})}
 
-                                {:form-type :list
-                                 :forms [(quote-mk-form form-type)
-                                         {:form-type :record
-                                          :forms (case form-type
-                                                   :string [(sym-form 'string) form]
-                                                   :bool [(sym-form 'bool) form]
+                                (:string :bool :int :float :big-int :big-float) {:form-type :quote,
+                                                                                 :form form}
 
-                                                   (:int :float :big-int :big-float) [(sym-form 'number) form]
+                                (:symbol :namespaced-symbol) (let [{:keys [ns sym]} (or (analyser/env-resolve form :vars env)
+                                                                                        {:ns (or (:ns form) current-ns),
+                                                                                         :sym (:sym form)})]
+                                                               (quoted-form :namespaced-symbol
+                                                                            {'ns {:form-type :quote,
+                                                                                  :form {:form-type :symbol
+                                                                                         :sym ns}}
+                                                                             'sym {:form-type :quote,
+                                                                                   :form {:form-type :symbol
+                                                                                          :sym sym}}}))
 
-                                                   (:symbol :namespaced-symbol) (let [{:keys [ns sym]} (or (analyser/env-resolve form :vars env)
-                                                                                                           {:ns (or (:ns form) current-ns),
-                                                                                                            :sym (:sym form)})]
-                                                                                  [(sym-form 'ns) {:form-type :quote,
-                                                                                                   :form (sym-form ns)}
-                                                                                   (sym-form 'sym) {:form-type :quote,
-                                                                                                    :form (sym-form sym)}])
+                                (quoted-form form-type
+                                             {'forms (let [splice? (some #(= :unquote-splicing (:form-type %)) forms)
+                                                           inner-forms {:form-type :vector
+                                                                        :forms (mapv #(syntax-quote-form % {:splice? splice?}) forms)}]
+                                                       (if splice?
+                                                         {:form-type :list
+                                                          :forms [{:form-type :namespaced-symbol
+                                                                   :ns 'bridje.forms
+                                                                   :sym 'concat}
+                                                                  inner-forms]}
 
-                                                   (:list :vector :set :record) [(sym-form 'forms) (let [splice? (some #(= :unquote-splicing (:form-type %)) forms)
-                                                                                                         inner-forms {:form-type :vector
-                                                                                                                      :forms (mapv #(syntax-quote-form % {:splice? splice?}) forms)}]
-                                                                                                     (if splice?
-                                                                                                       {:form-type :list
-                                                                                                        :forms [{:form-type :namespaced-symbol
-                                                                                                                 :ns 'bridje.forms
-                                                                                                                 :sym 'concat}
-                                                                                                                inner-forms]}
+                                                         inner-forms))}))]
 
-                                                                                                       inner-forms))])}]})]
               (if (and splice? (not= form-type :unquote-splicing))
                 {:form-type :vector
                  :forms [quoted-form]}
@@ -68,7 +69,7 @@
               (:vector :set :list :record) (update form :forms #(mapv expand-sq* %))
               :quote (update form :form expand-sq*)
 
-              :syntax-quote (syntax-quote-form (:form form) {:env env, :quote-type :syntax-quote})
+              :syntax-quote (syntax-quote-form (:form form) {:splice? false})
               :unquote (throw (ex-info "'unquote' outside of 'syntax-quote'" {:form form}))
               :unquote-splicing (throw (ex-info "'unquote-splicing' outside of 'syntax-quote'" {:form form}))))]
 
@@ -86,20 +87,21 @@
         (analyser/analyse env)
         (bridje.emitter/emit-value-expr env))))
 
-(let [env {:current-ns 'bridje.foo
-           :global-env {'bridje.forms {:vars {'->VectorForm {:value {}}
-                                              '->IntForm {:value {}}
-                                              '->ListForm {:value {}}
-                                              '->QuotedForm {:value {}}
-                                              '->StringForm {:value {}}
-                                              '->RecordForm {:value {}}
-                                              '->SymbolForm {:value {}}
-                                              '->NamespacedSymbolForm {:value {}}}}}}]
-  (-> (first (bridje.reader/read-forms "`foo"))
-      (expand-syntax-quotes env)
-      (analyser/analyse env)
-      (bridje.emitter/emit-value-expr env)))
+(comment
+  (let [env {:current-ns 'bridje.foo
+             :global-env {'bridje.forms {:vars {'->VectorForm {:value {}}
+                                                '->IntForm {:value {}}
+                                                '->ListForm {:value {}}
+                                                '->QuotedForm {:value {}}
+                                                '->StringForm {:value {}}
+                                                '->RecordForm {:value {}}
+                                                '->SymbolForm {:value {}}
+                                                '->NamespacedSymbolForm {:value {}}}}}}]
+    (-> (first (bridje.reader/read-forms "`foo"))
+        (expand-syntax-quotes env)
+        (analyser/analyse env)
+        (bridje.emitter/emit-value-expr env))))
 
 (comment
-  (-> (first (bridje.reader/read-forms "`[1 ~@['2 '3 '4]]"))
+  (-> (first (bridje.reader/read-forms "`[1 ~@[2 3 4]]"))
       (expand-syntax-quotes {})))
