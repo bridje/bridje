cb520a9670318562200c207b779738de0466eba4
James Henderson <james@jarohen.me.uk>
2017-11-22 22:56:35 +0000

    We're using the compositional type checker, remember?!

--- src/clj/bridje/type_checker.clj
+++ src/clj/bridje/type_checker.clj
@@ -1,153 +1,130 @@
-(ns bridje.type-checker)
+(ns bridje.type-checker
+  (:require [clojure.set :as set]))
+
+;; https://gergo.erdi.hu/projects/tandoori/Tandoori-Compositional-Typeclass.pdf
+
+(defn ->type-var [s]
+  {:type/type :type-var
+   :type/type-var (gensym (name s))})
+
+;; mono-env :: {Local -> Type}
+;; typing :: (mono-env, type)
+
+(defn mono-envs->type-equations [mono-envs]
+  (let [new-type-vars (into {}
+                            (comp (mapcat keys)
+                                  (distinct)
+                                  (map (fn [local]
+                                         [local (->type-var local)])))
+                            mono-envs)]
+    (into []
+          (mapcat (fn [mono-env]
+                    (for [[local mono-type] mono-env]
+                      [(get new-type-vars local)
+                       mono-type])))
+          mono-envs)))
 
 (do
-  (defn free-type-variables [type]
+  (defn apply-mapping [type mapping]
     (case (:type/type type)
-      :literal #{}
-      ))
+      (:int :float :big-int :big-float :string :bool) type
+      :type-var (get mapping (:type/type-var type) type)))
 
-  (defn substitute [type subst]
-    (case (:type/type type)
-      :literal type
-      :type-var (get subst (:type/type-var type) type)
-      :fn (-> type
-              (update :type/param-types (fn [params] (mapv #(substitute % subst) params)))
-              (update :type/return-type substitute subst))))
-
-  (defn substitute-scheme [{:keys [scheme/type-vars scheme/type] :as scheme} subst]
-    (-> scheme
-        (update :scheme/type substitute (apply dissoc subst type-vars))))
-
-  (defn substitute-env [type-env subst]
+  (defn mono-env-apply-mapping [mono-env mapping]
     (into {}
-          (map (fn [[local scheme]]
-                 (substitute-scheme scheme subst)))
-          type-env))
+          (map (fn [[local mono-type]]
+                 [local (apply-mapping mono-type mapping)]))
+          mono-env))
 
-  (defn instantiate [{:keys [scheme/type-vars scheme/type]}]
-    (substitute type
-                (into {}
-                      (map (fn [type-var]
-                             [type-var (gensym (name type-var))]))
-                      type-vars)))
+  (defn unify-eqs [eqs]
+    (loop [[[{t1-type :type/type, :as t1} {t2-type :type/type, :as t2} :as eq] & more-eqs] eqs
+           mapping {}]
+      (if-not eq
+        mapping
 
-  (defn merge-substs [& substs]
-    (reduce (fn [s1 s2]
-              (merge (into {}
-                           (map (fn [[tv type]]
-                                  [tv (substitute type s1)]))
-                           s2)
-                     s1))
-            substs))
+        (let [ex (ex-info "Cannot unify types" {:types #{t1 t2}})]
+          (cond
+            (and (= :type-var t1-type t2-type)
+                 (= t1 t2)) (recur more-eqs mapping)
 
-  (defn type-var [s]
-    {:type/type :type-var
-     :type/type-var (gensym (name s))})
+            (= :type-var t1-type)
+            (let [new-mapping {(:type/type-var t1) t2}]
+              (recur (map (fn [[t1 t2]]
+                            [(apply-mapping t1 new-mapping)
+                             (apply-mapping t2 new-mapping)])
+                          more-eqs)
 
-  (defn mgu [t1 t2]
-    (cond
-      (or (and (= :type-var (:type/type t1) (:type/type t2))
-               (= (:type/type-var t1) (:type/type-var t2)))
+                     (merge (into {}
+                                  (map (fn [[type-var mono-type]]
+                                         [type-var (apply-mapping mono-type new-mapping)]))
+                                  mapping)
+                            new-mapping)))
 
-          (and (= :literal
-                  (:type/type t1)
-                  (:type/type t2))
+            (= :type-var t2-type)
+            (recur (cons [t2 t1] more-eqs) mapping)
 
-               (= (:type/literal-type t1)
-                  (:type/literal-type t2))))
-      {}
+            (not= t1-type t2-type) (throw ex)
 
-      ;; TODO occurs checks
-      (= :type-var (:type/type t1))
-      {(:type/type-var t1) t2}
-
-      (= :type-var (:type/type t2))
-      {(:type/type-var t2) t1}
-
-      (and (= :fn (:type/type t1) (:type/type t2))
-           (= (count (:type/param-types t1))
-              (count (:type/param-types t2))))
-      (let [param-subst (reduce (fn [subst [t1-param t2-param]]
-                                  (merge-substs subst
-                                                (mgu (substitute t1-param subst)
-                                                     (substitute t2-param subst))))
-
-                                {}
-
-                                (map vector
-                                     (:type/param-types t1)
-                                     (:type/param-types t2)))
-
-            return-subst (mgu (substitute (:type/return-type t1) param-subst)
-                              (substitute (:type/return-type t2) param-subst))]
-        (merge-substs param-subst return-subst))
-
-      :else (throw (ex-info "Cannot unify types" {:types [t1 t2]}))))
+            :else (case (:type/type t1)
+                    (throw ex)))))))
 
   (defn type-expr [expr {:keys [env current-ns]}]
-    (letfn [(type-expr* [{:keys [expr-type] :as expr} {:keys [type-env]}]
+    (letfn [(type-expr* [{:keys [expr-type] :as expr}]
               (case expr-type
                 (:int :float :big-int :big-float :string :bool)
-                [{:type/type :literal, :type/literal-type expr-type} {}]
-
-                :fn
-                (let [param-type-vars (for [param (:locals expr)]
-                                        [param (type-var param)])
-
-                      [body-type subst] (type-expr* (:body-expr expr)
-                                                    {:type-env (merge type-env
-                                                                      (into {}
-                                                                            (map (fn [[local type-var]]
-                                                                                   [local {:scheme/type-vars []
-                                                                                           :scheme/type type-var}]))
-                                                                            param-type-vars))})]
-
-                  [{:type/type :fn
-                    :type/param-types (into [] (for [[_ param-type-var] param-type-vars]
-                                                 (substitute param-type-var subst)))
-                    :type/return-type body-type}
-
-                   subst])
-
-                :call
-                (let [return-var (type-var "ret")
-                      [fn-type fn-subst] (type-expr* (first (:exprs expr)) {:type-env type-env})
-                      [arg-types arg-subst] (reduce (fn [[arg-types arg-subst] arg-expr]
-                                                      (let [[arg-type arg-subst*] (type-expr* arg-expr (substitute-env type-env arg-subst))]
-                                                        [(conj arg-types arg-type) (merge-substs arg-subst* arg-subst)]))
-                                                    [[] fn-subst]
-                                                    (rest (:exprs expr)))
-                      unify-subst (mgu (substitute fn-type arg-subst)
-                                       {:type/type :fn
-                                        :type/param-types arg-types
-                                        :type/return-type return-var})]
-                  [(substitute return-var unify-subst) (merge-substs unify-subst arg-subst fn-subst)])
+                {:type/mono-env {}
+                 :type/mono-type {:type/type expr-type}}
 
                 :local
-                (if-let [local-type (get type-env (:local expr))]
-                  [(instantiate local-type) {}]
-                  (throw (ex-info "Unbound variable", {:local (:local expr)})))))]
+                (let [type-var (->type-var (:local expr))]
+                  {:type/mono-env {(:local expr) type-var}
+                   :type/mono-type type-var})
 
-      (when-let [[result-type subst] (type-expr* expr {:type-env {}})]
-        (substitute result-type subst))))
+                :vector
+                (let [elem-type-var (->type-var :elem)
+                      elem-typings (map type-expr* (:exprs expr))
+                      type-eqs (into (mono-envs->type-equations (map :type/mono-env elem-typings))
+                                     (into []
+                                           (comp (map :type/mono-type)
+                                                 (map (fn [elem-type]
+                                                        [elem-type elem-type-var])))
+                                           elem-typings))
+                      mapping (unify-eqs type-eqs)]
+
+                  ;; make type equations from all the mono-envs, and each elem type being equal to the elem-type-var
+                  ;; unify into a mapping
+                  ;; apply the mapping to each of the mono-envs
+                  ;; union the mono-envs, result.
+                  {:type/mono-env ::ohno
+                   :type/mono-type {:type/type :vector
+                                    :type/elem-type (apply-mapping elem-type-var mapping)}})
+
+                ))]
+
+      (type-expr* expr)))
 
   (for [expr (let [identity-fn {:expr-type :fn
                                 :sym :foo
                                 :locals [::foo-param]
                                 :body-expr {:expr-type :local
                                             :local ::foo-param}}]
-               [{:expr-type :int}
-                identity-fn
+               [#_{:expr-type :int}
+                #_{:expr-type :local
+                 :local ::foo}
 
-                {:expr-type :call,
-                 :exprs [identity-fn
-                         {:expr-type :int, :int 4}]}
+                {:expr-type :vector
+                 :exprs [{:expr-type :local, :local ::x}
+                         {:expr-type :int}]}
+                #_identity-fn
+
+                #_{:expr-type :call,
+                   :exprs [identity-fn
+                           {:expr-type :int, :int 4}]}
 
                 #_{:expr-type :if,
                    :pred-expr {:expr-type :bool, :bool false}
                    :then-expr {:expr-type :int, :int 4}
                    :else-expr {:expr-type :int, :int 5}}])
         ]
-    (type-expr expr {}))
-
+      (type-expr expr {}))
   )
