6b80d3deea6bfa0136c6c576d229ff0f614cd21e
James Henderson <james@jarohen.me.uk>
2017-11-24 14:07:29 +0000

    These are stable enough that I don't need to re-eval them each time

--- src/clj/bridje/type_checker.clj
+++ src/clj/bridje/type_checker.clj
@@ -24,73 +24,73 @@
                        mono-type])))
           mono-envs)))
 
+(defn apply-mapping [type mapping]
+  (case (:type/type type)
+    (:int :float :big-int :big-float :string :bool) type
+    :type-var (get mapping (:type/type-var type) type)))
+
+(defn mono-env-apply-mapping [mono-env mapping]
+  (into {}
+        (map (fn [[local mono-type]]
+               [local (apply-mapping mono-type mapping)]))
+        mono-env))
+
+(defn unify-eqs [eqs]
+  (loop [[[{t1-type :type/type, :as t1} {t2-type :type/type, :as t2} :as eq] & more-eqs] eqs
+         mapping {}]
+    (if-not eq
+      mapping
+
+      (let [ex (ex-info "Cannot unify types" {:types [t1 t2]})]
+        (cond
+          (= t1 t2) (recur more-eqs mapping)
+
+          (= :type-var t1-type)
+          (let [new-mapping {(:type/type-var t1) t2}]
+            (recur (map (fn [[t1 t2]]
+                          [(apply-mapping t1 new-mapping)
+                           (apply-mapping t2 new-mapping)])
+                        more-eqs)
+
+                   (merge (into {}
+                                (map (fn [[type-var mono-type]]
+                                       [type-var (apply-mapping mono-type new-mapping)]))
+                                mapping)
+                          new-mapping)))
+
+          (= :type-var t2-type)
+          (recur (cons [t2 t1] more-eqs) mapping)
+
+          (not= t1-type t2-type) (throw ex)
+
+          :else (case (:type/type t1)
+                  (throw ex)))))))
+
+(defn mono-env-union [mono-envs]
+  (->> mono-envs
+       (mapcat identity)
+       (reduce (fn [mono-env [local mono-type]]
+                 (if-let [existing-mono-type (get mono-env local)]
+                   (if (= existing-mono-type mono-type)
+                     mono-env
+                     (throw (ex-info "Can't unify types" {:local local
+                                                          :mono-types [mono-type existing-mono-type]})))
+                   (assoc mono-env local mono-type)))
+
+               {})))
+
+(defn combine-typings [{:keys [typings extra-eqs]}]
+  (let [mono-envs (map :type/mono-env typings)
+        mapping (unify-eqs (into (mono-envs->type-equations mono-envs)
+                                 extra-eqs))]
+
+    {:type/mono-env (->> mono-envs
+                         (map #(mono-env-apply-mapping % mapping))
+                         mono-env-union)
+
+     :type/mapping mapping}))
+
 (do
-  (defn apply-mapping [type mapping]
-    (case (:type/type type)
-      (:int :float :big-int :big-float :string :bool) type
-      :type-var (get mapping (:type/type-var type) type)))
-
-  (defn mono-env-apply-mapping [mono-env mapping]
-    (into {}
-          (map (fn [[local mono-type]]
-                 [local (apply-mapping mono-type mapping)]))
-          mono-env))
-
-  (defn unify-eqs [eqs]
-    (loop [[[{t1-type :type/type, :as t1} {t2-type :type/type, :as t2} :as eq] & more-eqs] eqs
-           mapping {}]
-      (if-not eq
-        mapping
-
-        (let [ex (ex-info "Cannot unify types" {:types [t1 t2]})]
-          (cond
-            (= t1 t2) (recur more-eqs mapping)
-
-            (= :type-var t1-type)
-            (let [new-mapping {(:type/type-var t1) t2}]
-              (recur (map (fn [[t1 t2]]
-                            [(apply-mapping t1 new-mapping)
-                             (apply-mapping t2 new-mapping)])
-                          more-eqs)
-
-                     (merge (into {}
-                                  (map (fn [[type-var mono-type]]
-                                         [type-var (apply-mapping mono-type new-mapping)]))
-                                  mapping)
-                            new-mapping)))
-
-            (= :type-var t2-type)
-            (recur (cons [t2 t1] more-eqs) mapping)
-
-            (not= t1-type t2-type) (throw ex)
-
-            :else (case (:type/type t1)
-                    (throw ex)))))))
-
-  (defn mono-env-union [mono-envs]
-    (->> mono-envs
-         (mapcat identity)
-         (reduce (fn [mono-env [local mono-type]]
-                   (if-let [existing-mono-type (get mono-env local)]
-                     (if (= existing-mono-type mono-type)
-                       mono-env
-                       (throw (ex-info "Can't unify types" {:local local
-                                                            :mono-types [mono-type existing-mono-type]})))
-                     (assoc mono-env local mono-type)))
-
-                 {})))
-
-  (defn combine-typings [{:keys [typings extra-eqs]}]
-    (let [mono-envs (map :type/mono-env typings)
-          mapping (unify-eqs (into (mono-envs->type-equations mono-envs)
-                                   extra-eqs))]
-
-      {:type/mono-env (->> mono-envs
-                           (map #(mono-env-apply-mapping % mapping))
-                           mono-env-union)
-
-       :type/mapping mapping}))
-
   (defn type-expr [expr {:keys [env current-ns]}]
     (letfn [(type-coll-expr [{:keys [expr-type] :as expr}]
               (let [elem-type-var (->type-var :elem)
