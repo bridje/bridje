2798fd875076398c27b8b11da1c6e92a319db128
James Henderson <james@jarohen.me.uk>
2017-11-27 17:12:41 +0000

    WIP: removing namespaces, and IO? I might regret this...

--- test/clj/bridje/main_test.clj
+++ test/clj/bridje/main_test.clj
@@ -1,6 +1,5 @@
 (ns bridje.main-test
-  (:require [bridje.main :as sut]
-            [bridje.test-util :refer [without-loc fake-file fake-io]]
+  (:require [bridje.test-util :refer [without-loc fake-forms]]
             [bridje.runtime :as rt]
             [bridje.compiler :as compiler]
             [clojure.string :as s]
@@ -9,115 +8,100 @@
   (:import [bridje.runtime ADT]))
 
 (t/deftest e2e-test
-  (let [fake-files {'bridje.kernel.foo (fake-file
-                                        '(ns bridje.kernel.foo)
+  (let [env (compiler/compile-str (fake-forms
+                                   '(def (flip x y)
+                                      [y x])
 
-                                        '(def (flip x y)
-                                           [y x])
+                                   '(defdata Nothing)
+                                   '(defdata (Just a))
+                                   '(defdata (Mapped #{a b}))
 
-                                        #_'(defmacro (if-not pred then else)
-                                             `(if ~pred
-                                                ~else
-                                                ~then))
+                                   '(def (main args)
+                                      (let [seq ["ohno"]
+                                            just (Just "just")
+                                            nothing Nothing]
+                                        {message (flip "World" "Hello")
+                                         seq seq
+                                         fn-call ((fn (foo a b) [b a]) "World" "Hello")
+                                         mapped (Mapped {a "Hello", b "World"})
+                                         the-nothing nothing
+                                         empty? ((clj empty?) seq)
+                                         just just
+                                         justtype (match just
+                                                    Just "it's a just"
+                                                    Nothing "it's nothing"
+                                                    "it's something else")
+                                         loop-rec (loop [x 5
+                                                         res []]
+                                                    (if ((clj zero?) x)
+                                                      res
+                                                      (recur ((clj dec) x)
+                                                             ((clj conj) res x))))
+                                         justval (Just->a just)})))
 
-                                        '(defdata Nothing)
-                                        '(defdata (Just a))
-                                        '(defdata (Mapped #{a b})))
+                                  {})]
 
-                    'bridje.kernel.bar (fake-file
-                                        '(ns bridje.kernel.bar
-                                           {aliases {foo bridje.kernel.foo}})
-
-                                        '(def (main args)
-                                           (let [seq ["ohno"]
-                                                 just (foo/Just "just")
-                                                 nothing foo/Nothing]
-                                             {message (foo/flip "World" "Hello")
-                                              seq seq
-                                              fn-call ((fn (foo a b) [b a]) "World" "Hello")
-                                              mapped (foo/Mapped {a "Hello", b "World"})
-                                              the-nothing nothing
-                                              empty? ((clj empty?) seq)
-                                              just just
-                                              justtype (match just
-                                                         foo/Just "it's a just"
-                                                         foo/Nothing "it's nothing"
-                                                         "it's something else")
-                                              loop-rec (loop [x 5
-                                                              res []]
-                                                         (if ((clj zero?) x)
-                                                           res
-                                                           (recur ((clj dec) x)
-                                                                  ((clj conj) res x))))
-                                              justval (foo/Just->a just)})))}
-
-        {:keys [compiler-io]} (fake-io {:source-files fake-files})
-        bootstrap-env (sut/bootstrap-env {:io compiler-io})]
-
-    (t/is (= (sut/run-main {:main-ns 'bridje.kernel.bar}
-                           {:io compiler-io, :env bootstrap-env})
+    (t/is (= (compiler/run-main env)
 
              {:message ["Hello" "World"],
               :fn-call ["Hello" "World"],
               :seq ["ohno"],
               :empty? false
-              :the-nothing (rt/->ADT 'bridje.kernel.foo/Nothing {}),
-              :mapped (rt/->ADT 'bridje.kernel.foo/Mapped {:a "Hello", :b "World"}),
-              :just (rt/->ADT 'bridje.kernel.foo/Just {:a "just"}),
+              :the-nothing (rt/->ADT 'Nothing {}),
+              :mapped (rt/->ADT 'Mapped {:a "Hello", :b "World"}),
+              :just (rt/->ADT 'Just {:a "just"}),
               :justtype "it's a just"
               :loop-rec [5 4 3 2 1]
               :justval "just"}))))
 
 (t/deftest quoting-test
-  (let [{:keys [compiler-io !compiled-files]} (fake-io {:source-files {'bridje.kernel.baz (s/join "\n" [(pr-str '(ns bridje.kernel.baz))
-                                                                                                        "(def simple-quote '(foo 4 [2 3]))"
-                                                                                                        "(def double-quote ''[foo 3])"
-                                                                                                        "(def syntax-quote `[1 ~'2 ~@['3 '4 '5]])"
-                                                                                                        (pr-str '(def (main args)
-                                                                                                                   {simple-quote simple-quote
-                                                                                                                    double-quote double-quote
-                                                                                                                    syntax-quote syntax-quote}))])}})
-        bootstrap-env (sut/bootstrap-env {:io compiler-io})]
+  (let [env (compiler/compile-str
+             (s/join "\n" [(pr-str '(ns bridje.kernel.baz))
+                           "(def simple-quote '(foo 4 [2 3]))"
+                           "(def double-quote ''[foo 3])"
+                           "(def syntax-quote `[1 ~'2 ~@['3 '4 '5]])"
+                           (pr-str '(def (main args)
+                                      {simple-quote simple-quote
+                                       double-quote double-quote
+                                       syntax-quote syntax-quote}))])
+             {})]
 
-    (t/is (= (-> (sut/run-main {:main-ns 'bridje.kernel.baz} {:io compiler-io, :env bootstrap-env})
+    (t/is (= (-> (compiler/run-main env)
                  (without-loc))
 
-             {:simple-quote (rt/->ADT 'bridje.kernel.forms/ListForm,
-                                      {:forms [(rt/->ADT 'bridje.kernel.forms/SymbolForm {:sym 'foo})
-                                               (rt/->ADT 'bridje.kernel.forms/IntForm {:number 4})
-                                               (rt/->ADT 'bridje.kernel.forms/VectorForm {:forms [(rt/->ADT 'bridje.kernel.forms/IntForm {:number 2})
-                                                                                                  (rt/->ADT 'bridje.kernel.forms/IntForm {:number 3})]})]})
+             {:simple-quote (rt/->ADT 'ListForm,
+                                      {:forms [(rt/->ADT 'SymbolForm {:sym 'foo})
+                                               (rt/->ADT 'IntForm {:number 4})
+                                               (rt/->ADT 'VectorForm {:forms [(rt/->ADT 'IntForm {:number 2})
+                                                                              (rt/->ADT 'IntForm {:number 3})]})]})
 
-              :double-quote (rt/->ADT 'bridje.kernel.forms/ListForm
-                                      {:forms [(rt/->ADT 'bridje.kernel.forms/NamespacedSymbolForm
-                                                         '{:ns bridje.kernel.forms, :sym VectorForm})
-                                               (rt/->ADT 'bridje.kernel.forms/RecordForm
-                                                         {:forms [(rt/->ADT 'bridje.kernel.forms/SymbolForm,
+              :double-quote (rt/->ADT 'ListForm
+                                      {:forms [(rt/->ADT 'SymbolForm
+                                                         '{:sym VectorForm})
+                                               (rt/->ADT 'RecordForm
+                                                         {:forms [(rt/->ADT 'SymbolForm,
                                                                             {:sym 'forms})
-                                                                  (rt/->ADT 'bridje.kernel.forms/VectorForm,
-                                                                            {:forms [(rt/->ADT 'bridje.kernel.forms/ListForm,
-                                                                                               {:forms [(rt/->ADT 'bridje.kernel.forms/NamespacedSymbolForm,
-                                                                                                                  '{:ns bridje.kernel.forms, :sym SymbolForm})
-                                                                                                        (rt/->ADT 'bridje.kernel.forms/RecordForm,
-                                                                                                                  {:forms [(rt/->ADT 'bridje.kernel.forms/SymbolForm,
+                                                                  (rt/->ADT 'VectorForm,
+                                                                            {:forms [(rt/->ADT 'ListForm,
+                                                                                               {:forms [(rt/->ADT 'SymbolForm '{:sym SymbolForm})
+                                                                                                        (rt/->ADT 'RecordForm,
+                                                                                                                  {:forms [(rt/->ADT 'SymbolForm,
                                                                                                                                      '{:sym sym})
-                                                                                                                           (rt/->ADT 'bridje.kernel.forms/ListForm,
-                                                                                                                                     {:forms [(rt/->ADT 'bridje.kernel.forms/NamespacedSymbolForm,
-                                                                                                                                                        '{:ns bridje.kernel.forms, :sym symbol})
-                                                                                                                                              (rt/->ADT 'bridje.kernel.forms/StringForm,
+                                                                                                                           (rt/->ADT 'ListForm,
+                                                                                                                                     {:forms [(rt/->ADT 'SymbolForm '{:sym symbol})
+                                                                                                                                              (rt/->ADT 'StringForm,
                                                                                                                                                         {:string "foo"})]})]})]})
-                                                                                     (rt/->ADT 'bridje.kernel.forms/ListForm,
-                                                                                               {:forms [(rt/->ADT 'bridje.kernel.forms/NamespacedSymbolForm,
-                                                                                                                  '{:ns bridje.kernel.forms, :sym IntForm})
-                                                                                                        (rt/->ADT 'bridje.kernel.forms/RecordForm,
-                                                                                                                  {:forms [(rt/->ADT 'bridje.kernel.forms/SymbolForm,
+                                                                                     (rt/->ADT 'ListForm,
+                                                                                               {:forms [(rt/->ADT 'SymbolForm '{:sym IntForm})
+                                                                                                        (rt/->ADT 'RecordForm,
+                                                                                                                  {:forms [(rt/->ADT 'SymbolForm,
                                                                                                                                      '{:sym number})
-                                                                                                                           (rt/->ADT 'bridje.kernel.forms/IntForm,
+                                                                                                                           (rt/->ADT 'IntForm,
                                                                                                                                      {:number 3})]})]})]})]})]})
 
-              :syntax-quote (rt/->ADT 'bridje.kernel.forms/VectorForm,
-                                      {:forms [(rt/->ADT 'bridje.kernel.forms/IntForm {:number 1})
-                                               (rt/->ADT 'bridje.kernel.forms/IntForm {:number 2})
-                                               (rt/->ADT 'bridje.kernel.forms/IntForm {:number 3})
-                                               (rt/->ADT 'bridje.kernel.forms/IntForm {:number 4})
-                                               (rt/->ADT 'bridje.kernel.forms/IntForm {:number 5})]})}))))
+              :syntax-quote (rt/->ADT 'VectorForm,
+                                      {:forms [(rt/->ADT 'IntForm {:number 1})
+                                               (rt/->ADT 'IntForm {:number 2})
+                                               (rt/->ADT 'IntForm {:number 3})
+                                               (rt/->ADT 'IntForm {:number 4})
+                                               (rt/->ADT 'IntForm {:number 5})]})}))))
