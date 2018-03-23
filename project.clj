345c61abd871d8e58f72734112de8413c7a3a653
James Henderson <james@jarohen.me.uk>
2018-03-23 22:02:35 +0000

    Let's go to deps.edn, because all the cool kids are doing it :)

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject jarohen/bridje "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.cli "0.3.5"]
-
-                 [instaparse "1.4.8"]]
-
-  :source-paths ["src/clj" "src/brj"]
-  :test-paths ["test/clj"])
