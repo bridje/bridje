d8c486e3e14e01cbd8f637b5e27b9da6a078f1ba
James Henderson <james@jarohen.me.uk>
2018-10-28 18:12:00 +0000

    It wants to add an IntelliJ file

--- test/brj/ActionExprTest.kt
+++ test/brj/ActionExprTest.kt
@@ -1,16 +1,24 @@
 package brj
 
+import brj.ActionExprAnalyser.TypeDefExpr
+import brj.BrjEnv.NSEnv
 import brj.Form.Companion.readForms
+import brj.Types.MonoType.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class ActionExprTest {
-    fun analyseActionExpr(brjEnv: BrjEnv = BrjEnv(), ns: Symbol = Symbol.create("USER"), form: Form): ActionExpr {
-        return ActionExpr.ActionExprAnalyser(brjEnv, ns).actionExprAnalyser(Analyser.AnalyserState(listOf(form)))
-    }
+    fun <R> analyse(a: (ActionExprAnalyser) -> FormsAnalyser<R>, forms: List<Form>, brjEnv: BrjEnv = BrjEnv(), nsEnv: NSEnv = NSEnv(Symbol.create("USER"))): R =
+        a(ActionExprAnalyser(brjEnv, nsEnv))(Analyser.AnalyserState(forms))
 
     @Test
     internal fun testTypedef() {
-        println(readForms("(:: foo Str)"))
-        println(analyseActionExpr(form = readForms("(:: foo Str)").first()))
+        assertEquals(
+            TypeDefExpr(Symbol.create("foo"), Types.Typing(StringType)),
+            analyse(ActionExprAnalyser::typeDefAnalyser, readForms("foo Str")))
+
+        assertEquals(
+            TypeDefExpr(Symbol.create("foo"), Types.Typing(FnType(listOf(StringType, IntType), StringType))),
+            analyse(ActionExprAnalyser::typeDefAnalyser, readForms("(foo Str Int) Str")))
     }
 }
