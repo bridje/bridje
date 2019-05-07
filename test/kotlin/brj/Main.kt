6678592e0cd8a89154cfbdca0eadde6bc6488bd0
James Henderson <james@jarohen.me.uk>
2019-05-07 07:55:08 +0100

    should really wrap the test in a try-finally

--- test/kotlin/brj/Main.kt
+++ test/kotlin/brj/Main.kt
@@ -7,20 +7,24 @@ import org.graalvm.polyglot.Source
 fun main() {
     val ctx = Context.newBuilder("bridje").allowAllAccess(true).build()
 
-    ctx.enter()
+    try {
+        ctx.enter()
 
-    val foo = Symbol.mkSym("foo")
-    val bar = Symbol.mkSym("bar")
+        val foo = Symbol.mkSym("foo")
+        val bar = Symbol.mkSym("bar")
 
-    val fooSource = Source.create("bridje", BridjeLanguage::class.java.getResource("main-foo.brj").readText())
+        val fooSource = Source.create("bridje", BridjeLanguage::class.java.getResource("main-foo.brj").readText())
 
-    val barSource = Source.create("bridje", BridjeLanguage::class.java.getResource("main-bar.brj").readText())
+        val barSource = Source.create("bridje", BridjeLanguage::class.java.getResource("main-bar.brj").readText())
 
-    val env = require(setOf(foo), mapOf(foo to fooSource, bar to barSource))
+        val env = require(setOf(foo), mapOf(foo to fooSource, bar to barSource))
 
-    val value = ctx.eval("bridje", """(str "Hello" " " "world!")""")
+        val value = ctx.eval("bridje", """(str "Hello" " " "world!")""")
 
-    println("value: $value")
+        println("value: $value")
+    } finally {
+        ctx.leave()
+        ctx.close(true)
+    }
 
-    ctx.leave()
 }
