c9b8b4ed257a3359fc25c8459f9bc9e2f44bad4e
James Henderson <james@jarohen.me.uk>
2021-03-14 00:22:06 +0000

    toDisplayString on Symbols

--- language/src/main/kotlin/brj/Symbol.kt
+++ language/src/main/kotlin/brj/Symbol.kt
@@ -1,12 +1,19 @@
 package brj
 
+import com.oracle.truffle.api.interop.InteropLibrary
 import com.oracle.truffle.api.interop.TruffleObject
+import com.oracle.truffle.api.library.ExportLibrary
+import com.oracle.truffle.api.library.ExportMessage
 
+@ExportLibrary(InteropLibrary::class)
 class Symbol private constructor(val local: String) : TruffleObject {
     companion object {
         private val SYMBOLS = mutableMapOf<String, Symbol>()
         internal fun symbol(local: String) = SYMBOLS.computeIfAbsent(local, ::Symbol)
     }
+
+    @ExportMessage
+    fun toDisplayString(allowSideEffects: Boolean) = local
 
     override fun toString() = local
 }
