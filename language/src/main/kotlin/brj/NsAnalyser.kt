71db32ddeec6e4cba80f49fab3c746dc2ff13d01
James Henderson <james@jarohen.dev>
2022-08-23 22:30:38 +0100

    WIP: starting to analyse `:imports`

--- language/src/main/kotlin/brj/NsAnalyser.kt
+++ language/src/main/kotlin/brj/NsAnalyser.kt
@@ -2,11 +2,39 @@ package brj
 
 import brj.runtime.BridjeContext
 import brj.runtime.NsContext
+import brj.runtime.Symbol
 import brj.runtime.Symbol.Companion.sym
+import com.oracle.truffle.api.interop.TruffleObject
 
 private val NS = "ns".sym
+private val IMPORTS = "imports".sym
+private val REFERS = "refers".sym
+private val ALIASES = "aliases".sym
 
 internal class NsAnalyser(private val env: BridjeContext) {
+
+    private fun Zip<Form>.analyseImports(): Map<Symbol, TruffleObject> {
+        val imports = mutableMapOf<Symbol, TruffleObject>()
+        val z = zright ?: TODO("expected form")
+        if (z.znode !is RecordForm) TODO("expected record")
+
+        var importZ = z.zdown
+
+        while (importZ != null) {
+            val packageSym = (importZ.znode as? SymbolForm)?.sym ?: TODO("expected symbol")
+            importZ = importZ.zright ?: TODO("missing form")
+            if (importZ.znode !is SetForm) TODO("expected set")
+
+            importZ.zdown.zrights.map {
+                val sym = (it.znode as? SymbolForm)?.sym ?: TODO("expected sym")
+                val import = "$packageSym.$sym"
+                env.truffleEnv.lookupHostSymbol(import) ?: TODO("can't find import: $import")
+            }
+
+            importZ = importZ.zright
+        }
+        return imports
+    }
 
     @Suppress("NAME_SHADOWING")
     internal fun analyseNs(z: Zip<Form>): NsContext {
@@ -17,7 +45,30 @@ internal class NsAnalyser(private val env: BridjeContext) {
         z = z.zright ?: TODO("expected ns symbol")
         val ns = ((z.znode as? SymbolForm) ?: TODO("expected symbol")).sym
 
-        return NsContext(env, ns)
+        var imports: Map<Symbol, TruffleObject>? = null
+
+        z.zright?.let {
+            if (it.znode !is RecordForm) TODO("expected record")
+            var optsZ = it.zdown
+            while (optsZ != null) {
+                val kw = (optsZ.znode as? KeywordForm)?.sym
+
+                optsZ = optsZ.zright ?: TODO("missing form")
+                when (kw) {
+                    IMPORTS -> imports = optsZ.analyseImports()
+                    ALIASES -> TODO()
+                    REFERS -> TODO()
+
+                    null -> TODO("expected keyword")
+                    else -> TODO("unexpected keyword ${kw}")
+                }
+
+                optsZ = optsZ.zright
+            }
+        }
+
+        return NsContext(env, ns, 
+            imports = imports ?: emptyMap())
     }
 }
 
