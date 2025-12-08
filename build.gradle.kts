e062888c8d098b8181fa2254454490d0612abf4e
James Henderson <james@jarohen.dev>
2025-12-08 22:57:10 +0000

    bump kotlin, jvm 23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.20" apply false
+    kotlin("jvm") version "2.1.0" apply false
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 rootProject.name = "bridje"
 include("language", "lsp", "tree-sitter", "gradle-plugin", "intellij")
