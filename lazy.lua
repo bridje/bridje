ca76fc86af129c5cf14e68f5a021f8d1ec3f9a7c
James Henderson <james@jarohen.dev>
2025-12-31 18:53:07 +0000

    feat(nvim): pulling treesitter down from bridje repo

--- lazy.lua
+++ lazy.lua
@@ -1,3 +1,18 @@
+local function register_parser()
+  local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+  if ok then
+    parsers.bridje = {
+      install_info = {
+        url = "https://github.com/bridje/bridje",
+        location = "tree-sitter",
+        generate = true,
+        generate_from_json = false,
+        queries = "tree-sitter/queries",
+      },
+    }
+  end
+end
+
 return {
   { "neovim/nvim-lspconfig", lazy = true },
   { "Olical/conjure", lazy = true },
@@ -6,6 +21,16 @@ return {
     ft = "bridje",
     init = function()
       vim.filetype.add({ extension = { brj = "bridje" } })
+      register_parser()
+      -- Re-register after TSUpdate (install reloads the parsers table)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = register_parser,
+      })
+    end,
+    build = function()
+      register_parser()
+      require("nvim-treesitter").install("bridje")
     end,
     config = function(plugin)
       vim.opt.runtimepath:append(plugin.dir .. "/nvim")
