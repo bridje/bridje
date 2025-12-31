4a46c2cc389eff6214727a40fc97a4d3da62749a
James Henderson <james@jarohen.dev>
2025-12-31 12:42:29 +0000

    feat(gradle): bridje has separate sourcesets

--- gradle-plugin/src/main/kotlin/brj/gradle/GradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/brj/gradle/GradlePlugin.kt
@@ -2,13 +2,18 @@ package brj.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
 
 interface BridjeExtension {
     val version: Property<String>
 }
+
+val SourceSet.bridje: SourceDirectorySet
+    get() = extensions.getByName("bridje") as SourceDirectorySet
 
 class GradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -16,13 +21,17 @@ class GradlePlugin : Plugin<Project> {
 
         project.plugins.apply("java")
 
-        val sourcesSets = project.extensions.getByName("sourceSets") as SourceSetContainer
+        val sourceSets = project.extensions.getByName("sourceSets") as SourceSetContainer
 
-        val mainSourceSet = sourcesSets.getByName("main")
-        mainSourceSet.java.srcDir("src/main/bridje")
+        sourceSets.all {
+            val bridjeSourceSet = project.objects.sourceDirectorySet("bridje", "$name Bridje source")
+            bridjeSourceSet.srcDir("src/$name/bridje")
+            bridjeSourceSet.filter.include("**/*.brj")
+            extensions.add("bridje", bridjeSourceSet)
+            resources.srcDir(bridjeSourceSet.srcDirs)
+        }
 
-        val testSourceSet = sourcesSets.getByName("test")
-        testSourceSet.java.srcDir("src/test/bridje")
+        val mainSourceSet = sourceSets.getByName("main")
 
         val lspConfig = project.configurations.create("bridjeLsp") {
             isCanBeConsumed = false
